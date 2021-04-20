517e2ee2cc46f5cc98c6620eec9f05d0d061128a
coffeetea99 <coffeetea99iro@gmail.com>
2021-04-20 21:42:53 +0900

    change public path and initialize public directory

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as cors from 'cors'
+import * as fs from 'fs';
 import db from './database';
 import route_diary from './router/route_diary';
 
@@ -8,7 +9,7 @@ app.set('port', process.env.port || 3009);
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('src/public'));
+app.use(express.static('public'));
 
 app.use('/diary', route_diary);
 
@@ -22,6 +23,12 @@ async function initialize() {
       )
   `;
   db.run(createTableQuery);
+
+  try {
+    fs.readdirSync('public/image');
+  } catch {
+    fs.mkdirSync('public/image', { recursive: true });
+  }
 
   console.log("Finish initialization");
 }
